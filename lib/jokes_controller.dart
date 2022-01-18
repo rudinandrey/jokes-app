0e7e12177cbaf989a94a2c06bd3fb74bba11ec67
rudinandrey <rudinandrey@yandex.ru>
2022-01-18 11:06:00 +0300

    загрузка анекдотов в контроллер при инициализации

--- lib/jokes_controller.dart
+++ lib/jokes_controller.dart
@@ -1,14 +1,35 @@
 import 'package:get/get.dart';
 import 'package:jokes/service.dart';
 
+import 'models.dart';
+
 class JokesController extends GetxController {
+  var jokes = RxList<Joke>().obs;
+  var joke = Joke(0, "").obs;
+
   @override
   void onInit() {
     super.onInit();
     print("init");
 
     Service s = Service();
-    var response = s.all("1");
-    print(response);
+    s.all("1").then((JokesResponse response) {
+      if (response.result == "success") {
+        jokes.value.addAll(response.jokes);
+        if (jokes.value.isNotEmpty) {
+          joke.value = jokes.value.removeAt(0);
+        } else {
+          joke.value = Joke(0, "Больше ничего нет");
+        }
+      }
+    });
+  }
+
+  void like() {
+    if (joke.value.id > 0) {}
+  }
+
+  void dislike() {
+    if (joke.value.id > 0) {}
   }
 }
