313797f99f08d5a398c4970dbccd7b40abf2d990
rudinandrey <rudinandrey@yandex.ru>
2022-01-18 11:06:42 +0300

    отображение анекдота на главном экране и привязка кнопок к контроллеру

--- lib/vote.dart
+++ lib/vote.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jokes/jokes_controller.dart';
 
 class Vote extends StatelessWidget {
-  const Vote({Key? key}) : super(key: key);
+  Vote({Key? key}) : super(key: key);
+
+  JokesController c = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Expanded(
+        Expanded(
           child: Padding(
             padding: EdgeInsets.all(16.0),
             child: SingleChildScrollView(
-              child: Text(
-                "hello world",
-                style: TextStyle(fontSize: 20),
+              child: Obx(
+                () => Text(
+                  c.joke.value.joke,
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
           ),
         ),
         Padding(
           padding: EdgeInsets.all(16),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Нравится"),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.green),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Не нравится"),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                ),
-              ),
-            ],
+          child: Obx(
+            () => c.joke.value.id != 0
+                ? Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          c.like();
+                        },
+                        child: Text("Нравится"),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.green),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          c.dislike();
+                        },
+                        child: Text("Не нравится"),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red),
+                        ),
+                      ),
+                    ],
+                  )
+                : Container(),
           ),
         )
       ],
